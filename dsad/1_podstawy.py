617e956502b1afdc9398e4af2ac2b82f87abde5f
Maksym Blachnierek <mblachnierek@edu.cdv.pl>
2020-03-05 17:20:30 +0100

    firsdsadadsad

---
+++ dsad/1_podstawy.py
@@ -0,0 +1,72 @@
+print("janusz")
+print(2)
+
+'''
+komentarz blokowy
+'''
+
+
+'''
+
+
+#potegowanie
+
+potega = 2 ** 10
+print(potega)
+#pobieranie danych z klawiatury
+name = input()
+
+print("twoje imie podane z klawiatury: " + name)
+
+surname = input()
+print("Imie: " + name + "Nazwisko: " + surname)
+
+print("Podaj swój wiek", end="")
+age= input ()
+
+age1= 20
+print(type(age1))
+print("Twój wiek: ",age1, "lata")
+
+
+'''
+
+###################################
+
+surname="Kowalski"
+firstChar = surname[0]
+print(firstChar)
+
+length=len(surname)
+print(length)
+
+lastChar=surname[len(surname)-1]
+print(lastChar)
+
+#konwersja
+x="5"
+
+print(type(x))
+
+x=int(x)
+print(type(x))
+
+
+y=4
+print(type(y))  #int
+
+y=y/2
+
+print(type(y))   #float
+print(y)
+
+
+
+
+
+
+
+
+
+
+
